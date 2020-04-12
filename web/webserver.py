00e018575c6635b314231edbf267b43232a0a3bc
Runfeng Xu <34244448+runfengxu@users.noreply.github.com>
2020-04-12 15:00:34 -0400

    sd

--- web/webserver.py
+++ web/webserver.py
@@ -1,5 +1,6 @@
-from flask import Flask
-from Image_recognition import predict
+from flask import Flask,request, render_template
+from werkzeug import secure_filename
+#from Image_recognition import predict
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,17 +12,19 @@ def display_result():
     if request.method =='POST':
         f = request.files['file']
         f.save(secure_filename(f.filename))
-        result = predict(f.filename)
-        recommendation = []
-        result = {}
-        for item in recommendation:
-            result[item]=db.get_img_url(item)
-        return render_template("dispaly.html",result = result)
-@app.route('/product/<asin>')
-def hello_name(asin):
-    metadata = db.get('metadata')
-    review= db.get('review')
-    return render_template('product.html',metadata = metadata,review = review)
+        print(f.filename)
+        return f.filename
+#         result = predict(f.filename)
+#         recommendation = []
+#         result = {}
+#         for item in recommendation:
+#             result[item]=db.get_img_url(item)
+#         return render_template("dispaly.html",result = result)
+# @app.route('/product/<asin>')
+# def hello_name(asin):
+#     metadata = db.get('metadata')
+#     review= db.get('review')
+#     return render_template('product.html',metadata = metadata,review = review)
 
 
 if __name__ == '__main__':
