1499504cf9f8a2d674ba555f191c3c4d48757664
Runfeng Xu <34244448+runfengxu@users.noreply.github.com>
2020-04-24 11:29:56 -0400

    sdsd

--- dataset/save_img.py
+++ dataset/save_img.py
@@ -1,32 +1,62 @@
 #import pandas as pd
 import gzip
 import urllib.request
-def parse(path):
-    g = gzip.open(path, 'rb')
-    for l in g:
-        yield eval(l)
+from multiprocessing import Pool
+import os
+import json
 
-def getDF(path):
-    j = 0
-    df = {}
-    d = parse(path)
-    while(True):
+class download():
+    def __init__(self,path):
+        self.path = path
+
+    def parse(self,size=1024*1024):
+        g = gzip.open(self.path, 'rb')
+        fileEnd = os.path.getsize(self.path)
+        chunkEnd = g.tell()
+        while True:
+            chunkStart = chunkEnd
+            g.seek(size,1)
+            g.readline()
+            chunkEnd = g.tell()
+
+            yield int(chunkStart),int(chunkEnd-chunkStart)
+            if chunkEnd>fileEnd:
+                break
+
+    def get_img(self,a):
         try:
-            a = next(d)
             if (('asin' in a) and ('imUrl' in a)):
                 asin = a['asin']
                 url = a['imUrl']
-                urllib.request.urlretrieve(url, 'img/'+asin+".jpg")
-                j += 1
-            if j%1000==0:
-                print(j)
-            else:
-                continue
-        except StopIteration:
-            print('finish')   
+                urllib.request.urlretrieve(url, 'E:/553/553project/dataset/img/'+asin+".jpg")
+                return 0
         except:
-            continue
+            return 1
+    def process_wrapper(self,chunkStart,chunkSize):
+        print('start chunk ',chunkStart)
+        with gzip.open(self.path) as f:
+            f.seek(chunkStart)
+            lines = f.read(chunkSize).splitlines()
+            for line in lines:
+                line = json.dumps(eval(line))
+                line = json.loads(line)
+                self.get_img(line)
 
-df = getDF('meta_Clothing_Shoes_and_Jewelry.json.gz')
+    def run_parallel(self,processes=8):
+        processes = int(processes)
+        pool = Pool(processes)
+        # try:
+        print(1)
+        jobs =[]
+        for chunkStart,chunkSize in self.parse():
+            jobs.append(pool.apply_async(self.process_wrapper,args=(chunkStart,chunkSize)))
+        for job in jobs:
+            job.get()
+            pool.close()
 
-    
+        # except Exception as e:
+        #     print(e)
+       
+if __name__ =='__main__':
+    case = download('E:/553/553project/dataset/meta_Clothing_Shoes_and_Jewelry.json.gz')
+    case.run_parallel(8)
